bbbc64288fea86dfa3b748df9acceaafc2af541a
angganurf <angganurfaizal234@gmail.com>
2023-08-15 16:46:39 +0700

    Update: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,41 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/public/api/:path*",
-        destination: "https://api.angganurf.com/:path*",
-      },
-    ];
+const withRemoveImports = require("next-remove-imports")();
+
+const path = require("path");
+
+const baseSecurityHeaders = [
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
   },
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "vjy.me",
-        port: "",
-        pathname: "/_next/**",
-      },
-    ],
+  {
+    key: "Cross-Origin-Opener-Policy",
+    value: "same-origin",
   },
-};
+];
+
+module.exports = withRemoveImports(
+  withBundleAnalyzer({
+    reactStrictMode: true,
+    swcMinify: true,
+    output: "standalone",
+    headers() {
+      return [
+        {
+          source: "/:path*",
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: `frame-ancestors 'self' http://localhost http://localhost:3000 https://angganurf.com https://*.angganurf.com https://api.angganurf.com ;`,
+            },
+            ...baseSecurityHeaders,
+          ],
+        },
+      ];
+    },
+  })
+);
