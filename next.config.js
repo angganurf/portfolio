7e03dc751eab6fe936ec9bc761bbb5111030ebbc
angganurf <angganurfaizal234@gmail.com>
2023-08-15 16:26:48 +0700

    Update: update CORS next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/public/api/:path*",
+        destination: "https://api.angganurf.com/:path*",
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
