eca152907f8adb1907aaed32d33888f6bf0e2214
angganurf <angganurfaizal234@gmail.com>
2023-08-15 18:01:36 +0700

    Update: update OG Image

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,13 +2,13 @@ export default {
   openGraph: {
     type: "website",
     locale: "en_IE",
-    title: "angganurf ✦ fullstack web developer, designer",
-    url: "https://angganurf.com",
+    title: "angganurf ✦ full stack web developer, designer",
+    url: "https://www.angganurf.com",
     description:
       "I am a fullstack web developer, currently looking into learning new things (Rust, Go, Web3 and backend technologies). Whenever I feel like I should share my knowledge with others, I like to do it via blog posts and social media.",
     locale: "en_EN",
     keywords:
-      "designer, product designer, full stack, indian designer, angga nurf, realvjy, illlustrations, indonesian developer, programmer sukabumi, sushi design system, zomato designer, 3dicons, uiprint, frontend developer, web developer, design india, web3, nft art",
+      "designer, product designer, full stack web, indonesia designer, angga nurf, angganurfaisal, illlustrations, indonesian developer, programmer sukabumi, ui/ux designer in sukabumi, indonesia sukabumi designer, full stack web, backend developer, frontend developer, web developer, design india, web3, nft art",
     images: [
       {
         width: 1200,
@@ -19,7 +19,7 @@ export default {
     site_name: "angganurf",
   },
   twitter: {
-    handle: "@gawwbtc",
+    handle: "@angganurfaisal",
     site: "angganurf.com",
     cardType: "summary_large_image",
   },
