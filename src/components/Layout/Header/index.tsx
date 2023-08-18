31cb2862089050dcc3028144825611ef73b834c2
Angga Nurf <31128877+angganurf@users.noreply.github.com>
2023-08-18 11:48:31 +0700

    Update index.tsx header

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -11,7 +11,7 @@ export default function Header() {
   const { asPath } = useRouter();
 
   useEffect(() => {
-    setShowNav(false);
+    setShowNav(showNav);
   }, [asPath]);
 
   const handleButton = () => {
